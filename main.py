8ab83c1b81f49df2dd751350f8488539f67000a6
Zaki Machfj <zakimachfj@gmail.com>
2022-10-23 02:16:05 -0400

    add check for command line argumetns

--- main.py
+++ main.py
@@ -1,42 +1,47 @@
 import requests
 import smtplib
 import os
+import sys
 from datetime import datetime
 from bs4 import BeautifulSoup as bsoup
 from players import get_players_from_league
 
-EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+SENDER = os.environ.get('EMAIL_USER')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
-
-
-myteam_id = 602008841651027968
-league_id = 851541694313775104
 injuries_url = "https://www.nfl.com/injuries/"
 
-my_players = [
-    f' {player} ' for player in get_players_from_league(myteam_id, league_id)
-]
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("USAGE: python3 main.py <SLEEPER_LEAGUE_ID> <SLEEPER_TEAM_ID> <EMAIL_ADDRESS>")
+    else:
+        league_id = sys.argv[1]
+        team_id = sys.argv[2]
+        receiver = sys.argv[3]
 
-html = requests.get(injuries_url).content
-soup = bsoup(html, 'html.parser')
-tags = soup.find_all('a', string = my_players)
-players = ','.join(tags)
+        players = [
+             f' {player} ' for player in get_players_from_league(team_id, league_id)
+        ]
 
-now = datetime.now()
-format = "%m/%d/%Y"
-today = now.strftime(format)
+        html = requests.get(injuries_url).content
+        soup = bsoup(html, 'html.parser')
+        tags = soup.find_all('a', string = players)
+        formattedPlayers = ','.join(tags)
 
-# sending email with SMTP, this process is automated externally with cron
-with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.ehlo()
+        now = datetime.now()
+        format = "%m/%d/%Y"
+        today = now.strftime(format)
 
-    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+            smtp.ehlo()
+            smtp.starttls()
+            smtp.ehlo()
 
-    subject = f'Injured Fantasy Players for {today}'
-    body = f'The following players are injured: {players}'
+        smtp.login(SENDER, EMAIL_PASSWORD)
 
-    msg = f'Subject: {subject}\n\n{body}'
+        subject = f'Injured Fantasy Players for {today}'
+        body = f'The following players are injured: {players}'
 
-    smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, msg)
+        msg = f'Subject: {subject}\n\n{body}'
+
+        smtp.sendmail(SENDER, receiver, msg)
+    
